0246cadc2bd4db2c47b72eab69fc2f790ef1f678
David Jensen <tsourdox@icloud.com>
2021-05-31 11:53:18 +0200

    add deploy script

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 3334;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!!!');
+    res.status(200).send('Hello World, automation is awesome!');
 })
 
 
